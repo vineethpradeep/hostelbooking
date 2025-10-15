6e0e98e28feff7dc2a2604f83aa6bb268effe3fc
vineethpradeep <165773642+vineethpradeep@users.noreply.github.com>
2025-10-15 10:26:50 +0100

    update the cards

--- src/app/components/featured-rooms/rooms.component.ts
+++ src/app/components/featured-rooms/rooms.component.ts
@@ -5,9 +5,9 @@ interface Room {
   img: string;
   title: string;
   price: number;
-  size: string;
+  propertyType: string;
   capacity: string;
-  bed: string;
+  features: string;
   services: string;
 }
 
@@ -24,37 +24,37 @@ export class FeaturedRoomsComponent {
       img: 'assets/img/room-bg-1.jpg',
       title: 'Single Occupancy',
       price: 8000,
-      size: '1 Bed',
+      propertyType: '1 Bed',
       capacity: 'Max 1 Person',
-      bed: 'Attached Bathroom, AC, Study Table, Wardrobe',
-      services: 'Book Now 🛏️',
+      features: 'Attached Bathroom, AC, Study Table, Wardrobe',
+      services: 'Book Now',
     },
     {
       img: 'assets/img/room-bg-2.jpg',
       title: 'Double Sharing',
       price: 5500,
-      size: '2 Beds',
+      propertyType: '2 Beds',
       capacity: 'Max 2 Persons',
-      bed: 'Shared Bathroom, AC, Study Tables, Wardrobes',
-      services: 'Book Now 🛏️',
+      features: 'Shared Bathroom, AC, Study Tables, Wardrobes',
+      services: 'Book Now',
     },
     {
       img: 'assets/img/room-bg-3.jpg',
       title: 'Triple Sharing',
       price: 4500,
-      size: '3 Beds',
+      propertyType: '3 Beds',
       capacity: 'Max 3 Persons',
-      bed: 'Shared Bathroom, Fan, Study Tables, Wardrobes',
-      services: 'Book Now 🛏️',
+      features: 'Shared Bathroom, Fan, Study Tables, Wardrobes',
+      services: 'Book Now',
     },
     {
       img: 'assets/img/room-bg-4.jpg',
       title: 'Four Sharing',
       price: 3500,
-      size: '4 Beds',
+      propertyType: '4 Beds',
       capacity: 'Max 4 Persons',
-      bed: 'Shared Bathroom, Fan, Study Tables, Wardrobes',
-      services: 'Book Now 🛏️',
+      features: 'Shared Bathroom, Fan, Study Tables, Wardrobes',
+      services: 'Book Now',
     },
   ];
 }
