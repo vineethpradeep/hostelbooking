85c2d605a8248e77f8d4f7d4e83d6837201e1d1e
Sabari <sabarimalai_alagar@api-hk.com>
2025-11-13 15:25:02 +0530

    space

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import {
   FormBuilder,
   FormGroup,
-  Validators,
+  Validators, 
   ReactiveFormsModule
 } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
